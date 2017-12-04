fc8770c49556afc869a1279eb0468975f0b5301b
Tim van der Lippe <tvanderlippe@gmail.com>
2017-12-04 13:02:34 +0100

    CoffeeScript -> JS

--- src/applause.js
+++ src/applause.js
@@ -1,21 +1,21 @@
-# Description:
-#   Applause from Orson Welles and others
-#
-# Dependencies:
-#   None
-#
-# Configuration:
-#   None
-#
-# Commands:
-#   hubot applause|applaud|bravo|slow clap - Get applause
-#   hubot sarcastic applause|clap - Get sarcastic applause
-#
-# Author:
-#   joshfrench, rjanardhana (ported to hubot-script package)
+// Description:
+//   Applause from Orson Welles and others
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//   hubot applause|applaud|bravo|slow clap - Get applause
+//   hubot sarcastic applause|clap - Get sarcastic applause
+//
+// Author:
+//   joshfrench, rjanardhana (ported to hubot-script package)
 
 
-images =
+const images = {
   sincere: [
     "http://i.imgur.com/pfrtv6H.gif",
     "http://i.imgur.com/Bp4P8l3.gif",
@@ -34,13 +34,13 @@ images =
     "http://i.imgur.com/9Zv4V.gif",
     "http://i.imgur.com/t8zvc.gif",
     "http://cache.blippitt.com/wp-content/uploads/2012/06/Daily-Life-GIFs-06-The-Rock-Clapping.gif",
-    "http://25.media.tumblr.com/tumblr_m00e9mCyWj1rqtbn0o1_500.gif"
+    "http://25.media.tumblr.com/tumblr_m00e9mCyWj1rqtbn0o1_500.gif",
     "http://assets0.ordienetworks.com/images/GifGuide/clapping/Kurtclapping.gif",
     "http://assets0.ordienetworks.com/images/GifGuide/clapping/riker.gif",
     "http://assets0.ordienetworks.com/images/GifGuide/clapping/hp3.gif",
     "http://assets0.ordienetworks.com/images/GifGuide/clapping/1292223254212-dumpfm-mario-Obamaclap.gif",
     "http://www.reactiongifs.com/wp-content/uploads/2013/01/applause.gif"
-  ]
+  ],
   insincere: [
     "http://i.imgur.com/2QXgcqP.gif",
     "http://i.imgur.com/Yih2Lcg.gif",
@@ -60,8 +60,11 @@ images =
     "http://i.imgur.com/Sx8iAS8.gif",
     "http://i.imgur.com/5zKXz.gif"
   ]
+};
 
-module.exports = (robot) ->
-  robot.respond /applau(d|se)|bravo|sarcastic applause|(slow|sarcastic) clap/i, (msg) ->
-    type = if (/sarcastic/i).test(msg.message.text) then images.insincere else images.sincere
-    msg.send msg.random type
+module.exports = robot =>
+  robot.respond(/applau(d|se)|bravo|sarcastic applause|(slow|sarcastic) clap/i, function(msg) {
+    const type = (/sarcastic/i).test(msg.message.text) ? images.insincere : images.sincere;
+    return msg.send(msg.random(type));
+  })
+;
